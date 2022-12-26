c5194ed1ed915b02fd2d4fa2ba1c849cf10947ed
tomislavvesic <tomislav.vesic1997@gmail.com>
2022-12-26 02:28:24 +0100

    add font awesome, center divs on intro component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { PersonalInfoState } from './core/state/personal-info.state';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { IntroComponent } from './portfolio/intro/intro.component';
+import { InfoComponent } from './portfolio/info/info.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +28,7 @@ import { provideDatabase, getDatabase } from '@angular/fire/database';
     NgxsModule.forRoot([PersonalInfoState]),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireStorageModule,
+    FontAwesomeModule,
 
     // Added by "ng add @angular/fire" command
     // provideFirebaseApp(() => initializeApp(environment.firebase)),
